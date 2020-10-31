3f36870425dab11830ad751d15daad724f567e82
sophieO1970 <sophieO1970@gmail.com>
2020-10-31 13:35:56 +0300

    add class quote,comment,blog and subscriber

--- app/models.py
+++ app/models.py
@@ -9,6 +9,16 @@ from datetime import datetime
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+class Quote:
+    '''
+    Quote class to define the quotes objects
+    
+    '''
+    
+    def __init__(self,author,quote):
+        self.author = author
+        self.quote = quote
+
   
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
@@ -19,6 +29,9 @@ class User(UserMixin,db.Model):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
+    blog = db.relationship('Blog', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
+
     
     @property
     def password(self):
@@ -35,3 +48,78 @@ class User(UserMixin,db.Model):
     def __repr__(self):
         return f'User {self.username}'
     
+    
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+        
+        
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(255),nullable=False)
+    content = db.Column(db.String(),nullable=False)
+    posted_on = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comment = db.relationship('Comment', backref='blog', lazy='dynamic')
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @classmethod
+    def get_blog(id):
+        blog = Blog.query.filter_by(id=id).first()
+
+        return blog
+
+    def __repr__(self):
+        return f"Blog ('{self.title}','{self.posted_on}')"
+
+
+class Comment(db.Model):
+    __tablename__='comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    blog_id = db.Column(db.Integer,db.ForeignKey("blogs.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.remove(self)
+        db.session.commit()
+
+    def get_comment(id):
+        comment = Comment.query.all(id=id)
+        return comment
+
+
+    def __repr__(self):
+        return f'Comment {self.comment}'
+
+class Subscriber(db.Model):
+    __tablename__='subscribers'
+
+    id=db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(255),unique=True,index=True)
+
+    def save_subscriber(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Subscriber {self.email}'
