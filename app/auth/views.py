a2a0c8e02ffb38efcef7147c7f301a3c2c10e599
sophieO1970 <sophieO1970@gmail.com>
2020-10-31 13:47:56 +0300

    add a photo folder

--- app/auth/views.py
+++ app/auth/views.py
@@ -23,7 +23,7 @@ def signup():
     if form.validate_on_submit():
         user = User(email = form.email.data, username = form.username.data, password = form.password.data)
         user.save_user()
-        mail_message("Welcome to Sole_blog","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to Sole_blog","email/welcome_subscriber",user.email,user=user)
         return redirect(url_for('auth.login'))
     return render_template('auth/signup.html', r_form = form)
 
